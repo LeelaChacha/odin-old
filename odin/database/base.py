9586c71903d3fba46090c2a627bb91a7139a9c8c
Chacha <hukumrajsinghdeora@gmail.com>
2021-05-05 01:27:17 +0200

    [wip] adds abstract methods to IDatabase

--- odin/database/base.py
+++ odin/database/base.py
@@ -1,9 +1,14 @@
+from abc import ABC, abstractmethod
+
+
 class IDatabase():
     def __init__(self):
         self.connection = None
 
+    @abstractmethod
     def _establish_connection(self):
         pass
 
+    @abstractmethod
     def _initialise_database(self):
         pass
