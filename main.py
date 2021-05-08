c737d20c26738c067a98f62583586b91d8e07ad2
Chacha <hukumrajsinghdeora@gmail.com>
2021-05-08 02:35:44 +0200

    fixes record path for CI

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from odin.record.local import LocalRecord
 
 
 if __name__ == "__main__":
-    database = LocalRecord()
+    database = LocalRecord(record_path="odin_record.db")
     database.pull_memos("APPOINTMENT_IN_EMAIL")
